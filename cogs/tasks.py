35f0cbecd465f559e8fc3db4e615c2410c11c549
TheWordSmith123 <60981700+TheWordSmith123@users.noreply.github.com>
2023-03-14 12:52:20 -0400

    Updated Tasks to Add Delay

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -2,8 +2,9 @@ import requests, json, sys
 import discord
 from discord.ext import commands, tasks
 
-priorLive = True
+priorLive = False
 currentLive = False
+delay = 0 
 class botTasks(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
@@ -12,19 +13,25 @@ class botTasks(commands.Cog):
     def cog_unload(self):
         self.submissionsCheck.cancel()
 
-    @tasks.loop(minutes=0.2)
+    @tasks.loop(minutes=5)
     async def submissionsCheck(self):
+        global delay
+        if delay:
+            delay -= 1
+            return
         global priorLive
         global currentLive
         channelName = 'inmoonwetrust'
         contents = requests.get('https://www.twitch.tv/' +channelName).content.decode('utf-8')
+        print(contents)
         if 'isLiveBroadcast' in contents: 
             currentLive=True
+            delay = 36
         else:
             currentLive=False
             priorLive=True
         if currentLive and priorLive:
-            streamChannel = self.bot.fetch_channel(1073867160230838372)
+            streamChannel = await self.bot.fetch_channel(1073867160230838372)
             await streamChannel.send("Moon is live! https://twitch.tv/inmoonwetrust @here")
             priorLive = False
 
